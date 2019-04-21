554a2604e3eeaf9dcf1c170cc42035fbdcfab2ba
mdanish15 <mdanish.work@gmail.com>
2019-04-21 20:14:59 +0200

    Customizing for Danish
    Removing last updates of Shalini

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!
 <html>
 <body>
-
-<h1>Shalini even your labs are due, kindly share at earliest</h1>
+  <h1> Completing my labs for Class 11-BeanStalk </h1>
+  <h2>Sample PHP page, forked from github/sohailadnan</h2>
 
 <?php
 echo "version :Ver 1.0";
