fc885c87713613df793a79a4cd77a1409b665f25
mdanish15 <mdanish.work@gmail.com>
2019-04-21 20:22:07 +0200

    Final release

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
   <h1> Completing my labs for Class 11-BeanStalk </h1>
   <h2>Sample PHP page, forked from github/sohailadnan</h2>
-
+  <h3> Thank you Sohail </h3>
 <?php
 echo "version :Ver 1.0";
 ?>
